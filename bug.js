0e3534ae1ccf1af410b6ebc65d1cf29c1d93e6cc
BugHunter-X <askzenai@gmail.com>
2024-12-05 02:02:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+The Firebase Realtime Database sometimes returns null values for certain data types, even when the data exists in the database.  This can occur inconsistently, making it difficult to debug. For example, a field that should contain a string might inexplicably return null.
